aa96626c98defef0050f53dc2d5bdfdb32c84893
kent426 <kenthuang426@gmail.com>
2022-06-06 15:23:34 +0800

    fixed algolia record size limit

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -16,19 +16,50 @@ const pageQuery = `{
   }
 }`
 
-function pageToAlgoliaRecord({ node: { id, title, path, ...rest } }) {
-  return {
-    objectID: id,
-    title,
-    path,
-    ...rest,
-  }
+function pageToAlgoliaRecord(edges) {
+  return edges.reduce(
+    (acc, { node: { id, title, path, content, ...rest } }) => {
+      if (Buffer.byteLength(content) > 90000) {
+        const strBound = Math.floor(content.length / 2)
+        return [
+          ...acc,
+          {
+            objectID: id,
+            title,
+            path,
+            content: content.slice(0, strBound),
+            ...rest,
+          },
+          {
+            objectID: id,
+            title,
+            path,
+            content: content.slice(strBound + 1),
+            ...rest,
+          },
+        ]
+      } else {
+        return [
+          ...acc,
+          {
+            objectID: id,
+            title,
+            path,
+            content,
+            ...rest,
+          },
+        ]
+      }
+    },
+    []
+  )
 }
 
 const queries = [
   {
     query: pageQuery,
-    transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
+    // transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
+    transformer: ({ data }) => pageToAlgoliaRecord(data.pages.edges),
     indexName,
     settings: { attributesToSnippet: [`excerpt:20`] },
   },
